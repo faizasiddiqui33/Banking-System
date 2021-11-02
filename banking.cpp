f9ba3f15b1ea070f06385e3692c3d35750fe39f6
faizasiddiqui33 <faiza.siddiqui33@gmail.com>
2021-11-01 21:04:15 -0600

    Adding Main Function

--- banking.cpp
+++ banking.cpp
@@ -45,3 +45,75 @@ public:
     //Default Constructor:
     Customer() {}
 
+}
+
+
+/*****************************************************************************************************************************
+Module Name: Setter Function for setting Customer's Name
+Author: Faiza Fatma Siddiqui
+Date Created/Modified: 01.11.2021
+Purpose: CS 700 - Software Development Fundamentals - Assignment 3
+Description: Set customer's name to the current value in pointer object (if not initialized by Constructor)
+    @param name: for passing customer's name as string data type
+    @return null
+*****************************************************************************************************************************/
+int main()
+{
+    Bank bank;
+    int choice;
+    double amount;
+    float fees = 0;
+    int accountType;
+    int accountNumber;
+    string date;
+    bool found;
+    do
+    {
+        try
+        {
+            cout << "\n\n\tEnter:\n\t 1 -- Add Account \n\t 2 -- Make Deposit \n\t 3 -- Make Withdrawal\n\t 4 -- Account Details \n\n\t Press any other key to Exit! \n\t >>  ";
+            cin >> choice;
+            switch (choice)
+            {
+            case 1:
+                bank.add_account();
+                break;
+         
+            case 2:
+                cout << "\tEnter Account Number : >> ";
+                cin >> accountNumber;
+                cout << "\tEnter the deposit Amount : >> $";
+                cin >> amount;
+                cout << "\tEnter the Date of Transaction(dd/mm/yyyy) : >> ";
+                cin >> date;
+                bank.make_deposit(amount, accountNumber, date, fees = fees);
+                break;
+
+            case 3:
+                cout << "\tEnter Account Number : >> ";
+                cin >> accountNumber;
+                cout << "\tEnter the WithDrawal Amount : >> $";
+                cin >> amount;
+                cout << "\tEnter the Date of Transaction(mm-dd-yyyy) : >> ";
+                cin >> date;
+                bank.make_withdrawal(amount, accountNumber, date, fees = fees);
+                break;
+
+            case 4:
+                cout << "\tEnter Account Number : >> ";
+                cin >> accountNumber;
+                found = bank.get_Account_number(accountNumber);
+                break;
+
+            default:
+                choice = 5;
+            }
+        }
+        catch (exception &x)
+        {
+            cout << "\t--------------------SOMETHING WENT WRONG! PLEASE TRY AGAIN! ----------" << endl;
+            choice = 5;
+        }
+    } while (choice != 5);
+    return 0;
+}
